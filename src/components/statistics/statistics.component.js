7716bf467443e16e393814d9e2da1a0bdb460873
sparksflyupwards <saad.jamil.khan@gmail.com>
2020-05-15 02:39:21 -0400

    statistics page now shows yearly/daily breakdown

--- src/components/statistics/statistics.component.js
+++ src/components/statistics/statistics.component.js
@@ -1,62 +1,59 @@
 import React, {Component} from 'react';
-import * as d3 from "d3";
 import Chart from "react-google-charts";
+import axios from "axios";
 
 class Statistics extends Component {
+    constructor(props){
+        super(props);
+        
+    
+        this.state = {sessions: [],
+                     data: [[{ type: 'date', id: 'Date' }, { type: 'number', id: 'minutesWorked' }],],
+                     dataIsReady: "true"  
+                    };
+      }
+
     componentDidMount() {
-        this.drawChart();
+        axios.get("http://localhost:5000/session/")
+        .then(res =>{
+            this.setState({sessions: res.data});
+            for (const session of this.state.sessions){
+                this.state.data.push([new Date(session.stop_time), Number(session.total_time/1000/60).toFixed(2)])
+            }
+            this.state.dataIsReady = "true"
+            console.log(this.state.data)
+        })
+        .catch(err=> {
+            console.log(err);
+        })
       }
         
-      drawChart() {
-         const w = 700;
-         const h = 500;
-        const data = [12, 5, 6, 6, 9, 100];
-        
-        const svg = d3.select("body")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .style("margin-left", 100);
-                      
-        svg.selectAll("rect")
-          .data(data)
-          .enter()
-          .append("rect")
-          .attr("x", (d, i) => i * 70)
-          .attr("y", (d, i) => h - 10 * d)
-          .attr("width", 65)
-          .attr("height", (d, i) => d * 10)
-          .attr("fill", "green")
-         
-          
-      }
+    
             
       render(){
-        return <div id={"#" + this.props.id}>
-    <Chart
-  width={'500px'}
-  height={'300px'}
-  chartType="Bar"
-  loader={<div>Loading Chart</div>}
-  data={[
-    ['Year', 'Sales', 'Expenses', 'Profit'],
-    ['2014', 1000, 400, 200],
-    ['2015', 1170, 460, 250],
-    ['2016', 660, 1120, 300],
-    ['2017', 1030, 540, 350],
-  ]}
-  options={{
-    // Material design options
-    chart: {
-      title: 'Company Performance',
-      subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-    },
-  }}
-  // For tests
-  rootProps={{ 'data-testid': '2' }}
-/>
+        return (<div id={"#" + this.props.id}> 
+    
 
-        </div>
+    {this.state.dataIsReady == "true" ? 
+    (
+    <Chart
+  width={1000}
+  height={350}
+  chartType="Calendar"
+  loader={<div>Loading Chart</div>}
+  data={this.state.data}
+  options={{
+    title: 'Yearly Data in minutes',
+  }}
+  rootProps={{ 'data-testid': '1' }}
+/>
+    ) :
+    (
+        <div>Fetching data from API</div>
+    )}
+
+
+        </div>);
       }
 }
 
