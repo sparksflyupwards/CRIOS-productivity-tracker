8baf792268b6c6ee830eac2776e8f19291b7efed
sparksflyupwards <saad.jamil.khan@gmail.com>
2020-05-23 01:26:20 -0400

    added login component

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import SessionsList from "./components/session/sessions-list.component";
 import NewSession from "./components/session/new-session.component";
 import Statistics from './components/statistics/statistics.component';
 import CreatureList from './components/creatures/creatures-list.component';
+import SignIn from './components/auth/SignIn.component';
 
 function App() {
   return (
     <Router>
       <div className="container">
+        
           <Navbar/>
           <br/>
           <Route path="/" exact component={SessionsList} />
+          <Route path="/login" exact component={SignIn} />
           <Route path="/edit/:id" exact component={NewSession} />
           <Route path="/create" exact component={NewSession} />
           <Route path="/user" exact component={NewSession} />   
